2b202a7f18664525439b6b90c51d60aef9a3d9f3
Keryljalbred <jalbredkeryldjeukouatchani@gmail.com>
2025-12-14 22:29:40 +0100

    fix: conftest2

--- tests/test_products.py
+++ tests/test_products.py
@@ -4,10 +4,10 @@ def test_add_product(client, auth_headers):
         json={
             "name": "Lait",
             "quantity": 1,
-            "expiry_date": "2025-12-31"
+            "expiration_date": "2025-12-31"  # ✅ conforme à l’API
         },
         headers=auth_headers
     )
-    print(response.json())
 
+    print(response.json())
     assert response.status_code in [200, 201]
